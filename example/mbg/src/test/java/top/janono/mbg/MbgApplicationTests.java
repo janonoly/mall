06d197b0d16e1c2c35c6f16b5cc94a5af30eb112
janono <1369021134@qq.com>
2021-04-26 20:01:51 +0800

    添加学习资料和示例

---
+++ example/mbg/src/test/java/top/janono/mbg/MbgApplicationTests.java
@@ -0,0 +1,13 @@
+package top.janono.mbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
